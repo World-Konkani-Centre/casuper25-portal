e343a7fd9fe7c510e313c313a44a1b5703463fe3
Praneeth <18praneeth@gmail.com>
2021-06-17 17:41:16 +0530

    Heroku error resolve

--- casuper25/settings.py
+++ casuper25/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_dir')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
